26307336fb0f7fcc9549abcc926ba8c49081e3c9
Alex Nahimana <alex.nahimana1@gmail.com>
2019-05-28 21:10:44 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -4,10 +4,10 @@ require 'pry'
 
 def fizzbuzz(num)
   if num % 3 == 0
-    num % 3
+    puts num % 3
     "Fizz"
   elsif num % 5 == 0
-    num % 5
+    puts num % 5
     "Buzz"
   elsif num % 3 == 0 && num % 5 == 0
     "FizzBuzz"
